8596ada538cfc06672e2c2cb4d5aa2452166a143
xmartina <martinsprince126@gmail.com>
2024-11-05 23:08:04 +0100

    d

--- cms/index.php
+++ cms/index.php
@@ -1,2 +1,6 @@
 <?php
+if (!isset($_SESSION['user_id']) && strpos($_SERVER['REQUEST_URI'], '/cms/login/') === false) {
+    header('Location: /cms/login/'); // Redirect to the login page
+    exit();
+}
 $rootDir = '/home/multistream6/domains/thomas.matagram.com/public_html/cms/';
