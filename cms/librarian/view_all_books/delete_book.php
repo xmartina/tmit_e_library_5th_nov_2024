6848b94034d32aea4c2da3374960d10533aec4bc
xmartina <martinsprince126@gmail.com>
2024-11-06 03:33:42 +0100

    d

--- cms/librarian/view_all_books/delete_book.php
+++ cms/librarian/view_all_books/delete_book.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include_once(__DIR__ . '/../../cms/functions/main_function.php'); // Include database connection file
+include_once(__DIR__ . '/../../../cms/functions/main_function.php'); // Include database connection file
 
 if (!isset($conn)) {
     die("Database connection error.");
