2ca1f5d751b7b7d4d3980f016e9082f322ed3223
xmartina <martinsprince126@gmail.com>
2024-11-06 01:01:45 +0100

    d

--- include/config.php
+++ include/config.php
@@ -4,3 +4,4 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once (__DIR__.'/../include/db_connect.php');
 include_once (__DIR__.'/../include/landing_db_connect.php');
+include_once (__DIR__.'/../include/portal_db_connect.php');
