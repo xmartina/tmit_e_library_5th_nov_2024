c31e992d32f1aef80840a0559d71eef1e8954de3
xmartina <martinsprince126@gmail.com>
2024-11-06 03:54:13 +0100

    d

--- cms/librarian/view_all_books/index.php
+++ cms/librarian/view_all_books/index.php
@@ -8,7 +8,7 @@ include_once(__DIR__ . '/../../../cms/partials/header.php');
 <div class="dashboard-main-body">
 
     <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
-        <h6 class="fw-semibold mb-0">Add E-Book</h6>
+        <h6 class="fw-semibold mb-0"><?=$page_name?></h6>
         <ul class="d-flex align-items-center gap-2">
             <li class="fw-medium">
                 <a href="<?=$site_url?>" class="d-flex align-items-center gap-1 hover-text-primary">
